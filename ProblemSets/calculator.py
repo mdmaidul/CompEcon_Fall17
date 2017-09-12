20c0095d820d8de4b9806a6606fb8b3df359c81e
maidul <mmic.e12@gmail.com>
2017-09-12 12:50:34 -0400

    Module for PS2 Jupyter Notebook

---
+++ ProblemSets/calculator.py
@@ -0,0 +1,14 @@
+def summation(val1, val2):
+    sumn = val1 + val2
+
+    return sumn
+
+def product(val1, val2):
+    prod = val1 * val2
+
+    return prod
+
+def squareroot(val):
+    sqrt = val**(1/2)
+
+    return sqrt
